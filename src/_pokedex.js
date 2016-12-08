1f6a777fb1b435b77930bbf0a2c384a47b2e02d7
Miles Rausch <miles@milesrausch.com>
2016-12-07 22:53:04 -0600

    Expand Pokédex a little bit

--- src/_pokedex.js
+++ src/_pokedex.js
@@ -1,23 +1,139 @@
 const pokedex = {
     miles: {
         key: 'miles',
-        name: 'Miles'
+        name: 'Miles',
+        evolvesInto: 'kilometers',
+        cp: 32,
+        portrait: 'miles.png',
+        stats: {
+            height: '2.02m',
+            type: 'fire',
+            weight: 'NaN'
+        },
+        description: 'This is the description.',
+        catch: {
+            location: 'Aberdeen, SD, United States',
+            date: '2/28/1983'
+        }
     },
-    miles_evolved: {
-        key: 'miles',
-        name: 'Kilometers'
+    kilometers: {
+        key: 'kilometers',
+        name: 'Kilometers',
+        evolvesInto: '',
+        cp: 33,
+        portrait: 'kilometers.png',
+        stats: {
+            height: '2.02m',
+            type: 'fire',
+            weight: 'NaN'
+        },
+        description: 'This is the description.',
+        catch: {
+            location: 'Aberdeen, SD, United States',
+            date: '2/28/1983'
+        }
     },
     holli: {
         key: 'holli',
-        name: 'Holli'
+        name: 'Holli',
+        evolvesInto: 'holliweather',
+        cp: 29,
+        portrait: 'holli.png',
+        stats: {
+            height: '2.02m',
+            type: 'fire',
+            weight: 'NaN'
+        },
+        description: 'This is the description.',
+        catch: {
+            location: 'Hawarden, SD, United States',
+            date: '4/15/1986'
+        }
+    },
+    holliweather: {
+        key: 'holliweather',
+        name: 'Holliweather',
+        evolvesInto: '',
+        cp: 30,
+        portrait: 'holliweather.png',
+        stats: {
+            height: '2.02m',
+            type: 'fire',
+            weight: 'NaN'
+        },
+        description: 'This is the description.',
+        catch: {
+            location: 'Hawarden, SD, United States',
+            date: '4/15/1986'
+        }
     },
     ian: {
         key: 'ian',
-        name: 'Ian'
+        name: 'Ian',
+        evolvesInto: 'bian',
+        cp: 5,
+        portrait: 'ian.png',
+        stats: {
+            height: '1m',
+            type: 'fire',
+            weight: '30'
+        },
+        description: 'This is the description.',
+        catch: {
+            location: 'Sioux Falls, SD, United States',
+            date: '4/30/2010'
+        }
+    },
+    bian: {
+        key: 'bian',
+        name: 'Bian',
+        evolvesInto: '',
+        cp: 6,
+        portrait: 'bian.png',
+        stats: {
+            height: '1m',
+            type: 'fire',
+            weight: '30'
+        },
+        description: 'This is the description.',
+        catch: {
+            location: 'Sioux Falls, SD, United States',
+            date: '4/30/2010'
+        }
     },
     ainsley: {
         key: 'ainsley',
-        name: 'Ainsley'
+        name: 'Ainsley',
+        evolvesInto: 'Budainsley',
+        cp: 3,
+        portrait: 'ainsley.png',
+        stats: {
+            height: '1m',
+            type: 'fire',
+            weight: 'NaN'
+        },
+        description: 'This is the description.',
+        catch: {
+            location: 'Sioux Falls, SD, United States',
+            date: '9/23/2012'
+        }
+    },
+    budainsley: {
+        key: 'budainsley',
+        name: 'Budainsley',
+        evolvesInto: '',
+        cp: 4,
+        portrait: 'budainsley.png',
+        stats: {
+            height: '1m',
+            type: 'fire',
+            weight: 'NaN'
+        },
+        description: 'This is the description.',
+        catch: {
+            location: 'Sioux Falls, SD, United States',
+            date: '9/23/2012'
+        }
     }
 };
 
