5670f2c7da01244cc4e7aaeaad46d03e7f1cb5f8
Miles Rausch <miles@milesrausch.com>
2016-12-10 11:22:57 -0600

    Fix tests for Pokemon component
    Guess I’m going this wrong.

--- src/Pokemon.test.js
+++ src/Pokemon.test.js
@@ -4,9 +4,19 @@ import Pokemon from './Pokemon';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  const data = {
-      name: 'Miles'
-  };
 
-  ReactDOM.render(<Pokemon pokemon={data} />, div);
+  ReactDOM.render(<Pokemon
+      cp="100"
+      portrait="image.png"
+      name="Miles"
+      stats={{
+          height: "1.0m",
+          type: "fire",
+          weight: "40km"
+      }}
+      description="This is a thing."
+      catch={{
+          location: "Sioux Falls",
+          date: "12/2/2016"
+      }} />, div);
 });
